675bb3a08a1197e641533a40a55c669c6fe8ff05
robbykraft <robbykraft@gmail.com>
2015-04-25 21:06:10 -0500

    user controls

--- readme.md
+++ readme.md
@@ -1,20 +1,21 @@
 # Platonic Solids
-* radius = 1.0
+* radius 1.0
 * centered at origin
 * one vertex aligned along +X
 * except hexahedron and dodecahedron, they are aligned to their duals (octa and icosa), they are face-aligned along +X
 * hexahedron and dodecahedron also come with triangle-only face arrays (OpenGL ES)
+
+![sample](https://raw.github.com/robbykraft/Platonic/master/sample.gif)
 
 # try it out
 * type `make` then `./world`
 * cross-platform Mac, Linux, Windows
 
 # input
+* `1`:tetra, `2`:octa, `3`:hexa, `4`:icosa, `5`:dodeca
+* `SPACE BAR` toggle lines/faces
 * mouse rotate
 * `W` `S` zoom in/out
 
-![sample](https://raw.github.com/robbykraft/Platonic/master/sample.gif)
-
 # license
-
 MIT
--- world.c
+++ world.c
@@ -13,116 +13,10 @@
 
 #include "platonic.h"
 
-// CHOOSE YOUR OWN PERSPECTIVE
+
+////////////////////////////////////////////////////////
+//       OpenGL ES draw functions for platonic.h
 //
-// 0: first person perspective, X Y movement
-// 1: polar, focus on origin, Y radius
-// 2: orthographic from above, X Y panning
-static int PERSPECTIVE = 1;
-// size of window in OS
-static int windowWidth = 800;
-static int windowHeight = 400;
-// INPUT HANDLING
-static unsigned int UP_PRESSED = 0;    // KEY UP:0   KEY DOWN:1
-static unsigned int DOWN_PRESSED = 0;
-static unsigned int RIGHT_PRESSED = 0;
-static unsigned int LEFT_PRESSED = 0;
-static int mouseDragOffsetX = 0;  // how far mouse is dragged during one session (between click and release)
-static int mouseDragOffsetY = 0;
-static int mouseTotalOffsetX = 0;  // how far mouse has been dragged since program began
-static int mouseTotalOffsetY = 0;
-// helpers
-static int mouseDragStartX = 0;
-static int mouseDragStartY = 0;
-static int mouseTotalOffsetStartX = 0;
-static int mouseTotalOffsetStartY = 0;
-
-#define STEP .10f  // WALKING SPEED. @60fps, walk speed = 6 units/second
-
-// POLAR PERSPECTIVE
-static float cameraRadius = 3.0f;
-// ORTHOGRAPHIC
-static float panX = 0.0f;
-static float panY = 0.0f;
-
-
-// LIGHTING
-static GLfloat light_position1[] = { 5.0, 5.0, 5.0, 0.0 };
-static GLfloat light_position2[] = { -5.0, 5.0, -5.0, 0.0 };
-static GLfloat light_position3[] = { -5.0, -5.0, 5.0, 0.0 };
-
-unsigned short selected_solid = 0;
-unsigned short seeThrough = 0;
-
-
-void init(){
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glDepthMask(GL_TRUE);
-	glClearDepth(1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-
-	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat mat_shininess[] = { 5.0f };
-	GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
-	GLfloat green[] = {0.0f, 1.0f, 0.0f, 1.0f};
-	GLfloat blue[] = {0.0f, 0.0f, 1.0f, 1.0f};
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_specular);
-	// glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, red);
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, blue);
-	glLightfv(GL_LIGHT2, GL_DIFFUSE, green);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position1);
-	glLightfv(GL_LIGHT1, GL_POSITION, light_position2);
-	glLightfv(GL_LIGHT2, GL_POSITION, light_position3);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHT1);
-	glEnable(GL_LIGHT2);
-	glCullFace(GL_FRONT);
-	glEnable(GL_CULL_FACE);
-	glShadeModel(GL_FLAT);
-	glShadeModel (GL_SMOOTH);
-	glLineWidth(5);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	mouseTotalOffsetY = 75;
-}
-
-void reshape(int w, int h){
-	windowWidth = w;
-	windowHeight = h;
-	float a = (float)windowWidth / windowHeight;
-	glViewport(0,0,(GLsizei) w, (GLsizei) h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	if(PERSPECTIVE == 1)
-		glFrustum (-.1, .1, -.1/a, .1/a, .1, 100.0);
-	else if (PERSPECTIVE == 2)
-		glOrtho(-3.0f, 3.0f, 
-				-3.0f/a, 3.0f/a, 
-				-100.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
-
-// draws a XY 1x1 square in the Z = 0 plane
-void unitSquare(float x, float y, float width, float height){
-	static const GLfloat _unit_square_vertex[] = { -0.5f, 0.5f, 0.0f,     0.5f, 0.5f, 0.0f,    -0.5f, -0.5f, 0.0f,    0.5f, -0.5f, 0.0f };
-	static const GLfloat _unit_square_normals[] = { 0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f  };
-	glPushMatrix();
-	glTranslatef(x, y, 0.0);
-	glScalef(width, height, 1.0);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_NORMAL_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, _unit_square_vertex);
-	glNormalPointer(GL_FLOAT, 0, _unit_square_normals);
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	glDisableClientState(GL_NORMAL_ARRAY);
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glPopMatrix();
-}
-
-// helper function for platonic.h
 void drawPlatonicSolidFaces(unsigned short solidType){
 	const float *vertices;
 	const unsigned short *faces;
@@ -160,9 +54,6 @@ void drawPlatonicSolidFaces(unsigned short solidType){
 	glDisableClientState(GL_NORMAL_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
-
-
-// helper function for platonic.h
 void drawPlatonicSolidLines(unsigned short solidType){
 	const float *vertices;
 	const unsigned short *lines;
@@ -200,7 +91,116 @@ void drawPlatonicSolidLines(unsigned short solidType){
 	glDisableClientState(GL_NORMAL_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
 
+
+
+// CHOOSE YOUR OWN PERSPECTIVE
+//
+// 0: first person perspective, X Y movement
+// 1: polar, focus on origin, Y radius
+// 2: orthographic from above, X Y panning
+static int PERSPECTIVE = 1;
+// size of window in OS
+static int windowWidth = 800;
+static int windowHeight = 400;
+// INPUT HANDLING
+static unsigned int UP_PRESSED = 0;    // KEY UP:0   KEY DOWN:1
+static unsigned int DOWN_PRESSED = 0;
+static unsigned int RIGHT_PRESSED = 0;
+static unsigned int LEFT_PRESSED = 0;
+static int mouseDragOffsetX = 0;  // how far mouse is dragged during one session (between click and release)
+static int mouseDragOffsetY = 0;
+static int mouseTotalOffsetX = 0;  // how far mouse has been dragged since program began
+static int mouseTotalOffsetY = 0;
+// helpers
+static int mouseDragStartX = 0;
+static int mouseDragStartY = 0;
+static int mouseTotalOffsetStartX = 0;
+static int mouseTotalOffsetStartY = 0;
+
+// POLAR PERSPECTIVE
+static float cameraRadius = 3.0f;
+// ORTHOGRAPHIC
+static float panX = 0.0f;
+static float panY = 0.0f;
+// LIGHTING
+static GLfloat light_position1[] = { 5.0, 5.0, 5.0, 0.0 };
+static GLfloat light_position2[] = { -5.0, 5.0, -5.0, 0.0 };
+static GLfloat light_position3[] = { -5.0, -5.0, 5.0, 0.0 };
+
+#define STEP .10f  // WALKING SPEED. @60fps, walk speed = 6 units/second
+
+unsigned short selected_solid = 0;
+unsigned short seeThrough = 0;
+
+
+void init(){
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glDepthMask(GL_TRUE);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_shininess[] = { 5.0f };
+	GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
+	GLfloat green[] = {0.0f, 1.0f, 0.0f, 1.0f};
+	GLfloat blue[] = {0.0f, 0.0f, 1.0f, 1.0f};
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_specular);
+	// glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, red);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, blue);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, green);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position1);
+	glLightfv(GL_LIGHT1, GL_POSITION, light_position2);
+	glLightfv(GL_LIGHT2, GL_POSITION, light_position3);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT2);
+	glCullFace(GL_FRONT);
+	glEnable(GL_CULL_FACE);
+	glShadeModel(GL_FLAT);
+	glShadeModel (GL_SMOOTH);
+	glLineWidth(5);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void reshape(int w, int h){
+	windowWidth = w;
+	windowHeight = h;
+	float a = (float)windowWidth / windowHeight;
+	glViewport(0,0,(GLsizei) w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if(PERSPECTIVE == 1)
+		glFrustum (-.1, .1, -.1/a, .1/a, .1, 100.0);
+	else if (PERSPECTIVE == 2)
+		glOrtho(-3.0f, 3.0f, 
+				-3.0f/a, 3.0f/a, 
+				-100.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+// draws a XY 1x1 square in the Z = 0 plane
+void unitSquare(float x, float y, float width, float height){
+	static const GLfloat _unit_square_vertex[] = { -0.5f, 0.5f, 0.0f,     0.5f, 0.5f, 0.0f,    -0.5f, -0.5f, 0.0f,    0.5f, -0.5f, 0.0f };
+	static const GLfloat _unit_square_normals[] = { 0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f,     0.0f, 0.0f, 1.0f  };
+	glPushMatrix();
+	glTranslatef(x, y, 0.0);
+	glScalef(width, height, 1.0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, _unit_square_vertex);
+	glNormalPointer(GL_FLOAT, 0, _unit_square_normals);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glPopMatrix();
+}
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -250,22 +250,6 @@ void display(){
 	// bring back buffer to the front on vertical refresh, auto-calls glFlush
 	glutSwapBuffers();
 	// glFlush();
-
-	// UPDATE
-
-	mouseTotalOffsetX += 1.66;
-	if(mouseTotalOffsetX % 180 < 180/5.0)   selected_solid = 0;
-	else if(mouseTotalOffsetX % 180 < 180/5.0 * 2)   selected_solid = 1;
-	else if(mouseTotalOffsetX % 180 < 180/5.0 * 3)   selected_solid = 2;
-	else if(mouseTotalOffsetX % 180 < 180/5.0 * 4)   selected_solid = 3;
-	else   selected_solid = 4;
-
-	if(((int)(mouseTotalOffsetX / 180.0)) % 2)
-		seeThrough = 1;
-	else
-		seeThrough = 0;
-	
-	glutPostRedisplay();
 }
 
 // process input devices if in orthographic mode
@@ -337,12 +321,6 @@ void keyboardDown(unsigned char key, int x, int y){
 	else if(key == '3') selected_solid = 2;
 	else if(key == '4') selected_solid = 3;
 	else if(key == '5') selected_solid = 4;
-	// else if(key == '1'){
-	// 	// PERSPECTIVE = 0;
-	// 	// mouseTotalOffsetX = mouseTotalOffsetY = 0;
-	// 	reshape(windowWidth, windowHeight);
-	// 	glutPostRedisplay();
-	// }
 	else if(key == 'p'){
 		PERSPECTIVE = 1;
 		// mouseTotalOffsetX = mouseTotalOffsetY = 0;
